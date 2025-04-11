5665c68232fcd17037661958767a1ca0767a5cae
borislav-rakic <rakic-borislav@hotmail.com>
2025-04-11 20:54:04 +0200

    Finish BinaryTreeMaxPath

--- assignment02/BinaryTreeMaxPath/src/main/java/zest/BinaryTreeMaxPath.java
+++ assignment02/BinaryTreeMaxPath/src/main/java/zest/BinaryTreeMaxPath.java
@@ -12,6 +12,11 @@ public class BinaryTreeMaxPath {
         }
 
         public static TreeNode constructTree(Integer[] array) {
+            // If the array is empty, a tree is returned with the root value set to Integer.MIN_VALUE
+            if (array.length == 0) {
+                return new TreeNode(Integer.MIN_VALUE);
+            }
+
             TreeNode[] nodes = new TreeNode[array.length];
             for (int i = 0; i < array.length; i++) {
                 if (array[i] != null) {
@@ -35,6 +40,17 @@ public class BinaryTreeMaxPath {
         if (root == null) {
             throw new IllegalArgumentException("The tree root must not be null.");
         }
+
+        // If root.val is equal to Integer.MIN_VALUE, the input array was empty, and thus the maxPathSum is 0
+        if (root.val == Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        // Pre-conditions
+        if (treeSize(root) > 1023) {
+            throw new IllegalArgumentException();
+        }
+
         maxPathSum = Integer.MIN_VALUE;
         maxGain(root);
         return maxPathSum;
@@ -53,5 +69,31 @@ public class BinaryTreeMaxPath {
         maxPathSum = Math.max(maxPathSum, priceNewPath);
 
         return node.val + Math.max(leftGain, rightGain);
+    }
+
+    // Calculates the amount of nodes in the tree
+    private int treeSize(TreeNode node) {
+        int size = 1;
+
+        if (node.left != null) {
+            // If a value is smaller than -10000 or bigger than 10000, a pre-condition doesn't hold
+            if (node.left.val < -10000 || node.left.val > 10000) {
+                throw new IllegalArgumentException();
+            }
+            size += treeSize(node.left);
+        }
+        if (node.right != null) {
+            // If a value is smaller than -10000 or bigger than 10000, a pre-condition doesn't hold
+            if (node.right.val < -10000 || node.right.val > 10000) {
+                throw new IllegalArgumentException();
+            }
+            size += treeSize(node.right);
+        }
+
+        return size;
+    }
+
+    public void setMaxPathSum(int newMaxPathSum) {
+        this.maxPathSum = newMaxPathSum;
     }
 }
--- assignment02/BinaryTreeMaxPath/src/test/java/zest/BinaryTreeMaxPathTest.java
+++ assignment02/BinaryTreeMaxPath/src/test/java/zest/BinaryTreeMaxPathTest.java
@@ -1,9 +1,178 @@
 package zest;
 
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BinaryTreeMaxPathTest {
+    BinaryTreeMaxPath calculator = new BinaryTreeMaxPath();
 
+    public BinaryTreeMaxPath.TreeNode setup(Integer[] input) {
+        calculator.setMaxPathSum(Integer.MIN_VALUE);
+        Integer[] treeArray = input;
+        BinaryTreeMaxPath.TreeNode root = BinaryTreeMaxPath.TreeNode.constructTree(treeArray);
 
+        return root;
+    }
 
+    @Test
+    public void testMaxPathSuccess() {
+        Integer[] array = {1, 2, 3};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        int expected = 6;
+
+        int result = calculator.maxPathSum(root);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMaxPathNullElementsSuccess() {
+        Integer[] array = {-10, 9, 20, null, null, 15, 7};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        int expected = 42;
+
+        int result = calculator.maxPathSum(root);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMaxPathIllegalPositiveNodeLeft() {
+        Integer[] array = {-10, 10001, 20, null, null, 15, 7};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Test
+    public void testMaxPathIllegalNegativeNodeLeft() {
+        Integer[] array = {-10, -10001, 20, null, null, 15, 7};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Test
+    public void testMaxPathIllegalPositiveNodeRight() {
+        Integer[] array = {-10, 9, 10001, null, null, 15, 7};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Test
+    public void testMaxPathIllegalNegativeNodeRight() {
+        Integer[] array = {-10, 9, -10001, null, null, 15, 7};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Test
+    public void testMaxPathEmptyList() {
+        Integer[] array = {};
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        int expected = 0;
+
+        int result = calculator.maxPathSum(root);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMaxPathNullRoot() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(null),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Test
+    public void testMaxPath1024Array() {
+        Integer[] input = new Integer[1024];
+
+        for (int i = 0; i < 1024; i++) {
+            input[i] = i;
+        }
+
+        BinaryTreeMaxPath.TreeNode root = setup(input);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Property
+    public void testIllegalNodeLeft(@ForAll @IntRange(min = 10001) Integer illegalPositive, @ForAll @IntRange(min = Integer.MIN_VALUE, max = -10001) Integer illegalNegative) {
+        Random random = new Random();
+        int randomInt = random.nextInt(2);
+
+        Integer[] array = {1, 2, 9};
+
+        if (randomInt == 0) {
+            array[1] = illegalPositive;
+        } else {
+            array[1] = illegalNegative;
+        }
+
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
+
+    @Property
+    public void testIllegalNodeRight(@ForAll @IntRange(min = 10001) Integer illegalPositive, @ForAll @IntRange(min = Integer.MIN_VALUE, max = -10001) Integer illegalNegative) {
+        Random random = new Random();
+        int randomInt = random.nextInt(2);
+
+        Integer[] array = {1, 2, 9};
+
+        if (randomInt == 0) {
+            array[2] = illegalPositive;
+        } else {
+            array[2] = illegalNegative;
+        }
+
+        BinaryTreeMaxPath.TreeNode root = setup(array);
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.maxPathSum(root),
+                "Expected IllegalArgumentException to be thrown"
+        );
+    }
 }
